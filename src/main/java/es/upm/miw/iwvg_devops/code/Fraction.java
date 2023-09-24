d35a446d76d13c0358ea62a229d2f8716a56355f
jiaan <chengjiaan123@gmail.com>
2023-09-24 20:20:33 +0200

    added more methods in Fraction #12

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -33,6 +33,35 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
+    public boolean isProper() {
+        return this.numerator < this.denominator;
+    }
+
+    public boolean isImproper() {
+        return this.numerator > this.denominator;
+    }
+
+    public boolean isEquivalent(Fraction fraction) {
+        return this.numerator*fraction.getDenominator() == this.denominator* fraction.getNumerator();
+    }
+
+    public Fraction add(Fraction fraction) {
+        int commonDenominator = this.denominator * fraction.getDenominator();
+        int numerator1 = commonDenominator/this.denominator*this.numerator;
+        int numerator2 = commonDenominator/fraction.getDenominator()*fraction.getNumerator();
+        return new Fraction(numerator1+numerator2, commonDenominator);
+    }
+
+    public Fraction multiply(Fraction fraction) {
+        return new Fraction(this.numerator* fraction.getNumerator(),
+                this.denominator* fraction.getDenominator());
+    }
+
+    public Fraction divide(Fraction fraction) {
+        return new Fraction(this.numerator* fraction.getDenominator(),
+                this.denominator* fraction.getNumerator());
+    }
+
     @Override
     public String toString() {
         return "Fraction{" +
