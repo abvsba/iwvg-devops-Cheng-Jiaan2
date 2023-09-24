d8e57fef328585b242ccb01d44ca0b059a9db575
jiaan <chengjiaan123@gmail.com>
2023-09-24 19:30:05 +0200

    added UserTest class #8

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -1,4 +1,10 @@
 package es.upm.miw.iwvg_devops.code;
 
 public class Fraction {
+    private int denominator;
+    private int numerator;
+    public Fraction(int denominator, int numerator) {
+        this.denominator = denominator;
+        this.numerator = numerator;
+    }
 }
